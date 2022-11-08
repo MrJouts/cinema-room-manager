1a0b0f774eeeabbb40dc7439229e2633391eae92
Emiliano <emiliano.hotes@despegar.com>
2022-11-08 17:53:26 -0300

    Arrangement - finished

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,5 +1,14 @@
-git spackage cinema
+package cinema
 
 fun main() {
-    // write your code here
+    println("Cinema:")
+    print("  ")
+    println("  1 2 3 4 5 6 7 8")
+    println("1 S S S S S S S S")
+    println("2 S S S S S S S S")
+    println("3 S S S S S S S S")
+    println("4 S S S S S S S S")
+    println("5 S S S S S S S S")
+    println("6 S S S S S S S S")
+    println("7 S S S S S S S S")
 }
