9013a4d55ce27c3b05f6527f8b28e73edbfc30d7
Emiliano <emiliano.hotes@despegar.com>
2022-11-09 15:37:43 -0300

    Ranges

---
+++ Topics/Ranges/Between two numbers/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val n = readln().toInt()
+    val range = readln().toInt()..readln().toInt()
+    println(n in range)
+}
---
+++ Topics/Ranges/Not in range/src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val n = readln().toInt()
+    println(n !in 1..10)
+}
