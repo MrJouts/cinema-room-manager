daa3a8684cb120d13429d7be8091276a71c84b06
Emiliano <emiliano.hotes@despegar.com>
2022-11-11 21:26:30 -0300

    Multi-dimensional list

---
+++ Topics/Multi-dimensional list/Corners/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Do not touch code below
+    val inputList: MutableList<MutableList<String>> = mutableListOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toMutableList()
+        inputList.add(strings)
+    }
+
+    println("${inputList[0][0]} ${inputList[0][inputList.lastIndex]}")
+    println("${inputList[inputList.lastIndex][0]} ${inputList[inputList.lastIndex][inputList.lastIndex]}")
+
+}
